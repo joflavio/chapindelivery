2e68d95e50488168dedcf67d41063566f6838699
joflavio <82711464+joflavio@users.noreply.github.com>
2023-09-30 16:07:12 -0600

    image folder fix
    image folder fix

--- backend/api/ecosystem.config.js
+++ backend/api/ecosystem.config.js
@@ -6,7 +6,8 @@ module.exports = {
           "NODE_ENV": "production",
           "INIT_DB":"false",
           "PORT":"8626",
-          "TOKEN_KEY":"i8o65TQIddw5vgsWizQZuQU0WZsb76FOaeLDKXgdZUS5611Y6rRhXI0Yguf9FhBfZ3UBBdvAQgOBbfkY"
+          "TOKEN_KEY":"i8o65TQIddw5vgsWizQZuQU0WZsb76FOaeLDKXgdZUS5611Y6rRhXI0Yguf9FhBfZ3UBBdvAQgOBbfkY",
+          "IMAGE_FOLDER":"uploads/"
       }
     }]
   }
